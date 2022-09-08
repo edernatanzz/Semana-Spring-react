1794739ee84cb0ff7709fcec7e52a77859104f2c
Eder Natan <edernatan124@gmail.com>
2022-09-08 15:50:30 -0300

    Date select

--- backend/src/main/java/com/devedernatan/dsmeta/repositories/SalesRepository.java
+++ backend/src/main/java/com/devedernatan/dsmeta/repositories/SalesRepository.java
@@ -1,11 +1,19 @@
 package com.devedernatan.dsmeta.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devedernatan.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
 
 //classe responsavel pelo banco de dados
 
 public interface SalesRepository extends JpaRepository <Sale , Long> {
-
+   
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
+	
 }
